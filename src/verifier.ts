49274335e814e4d43680df82265031c09fe4be5b
WMXPY <wm@mengw.io>
2020-04-24 12:46:19 -0500

    add verifier

---
+++ src/verifier.ts
@@ -0,0 +1,17 @@
+/**
+ * @author WMXPY
+ * @namespace Verify
+ * @description Verifier
+ */
+
+export class Verifier {
+
+    public static create(): Verifier {
+
+        return new Verifier();
+    }
+
+    private constructor() {
+
+    }
+}
