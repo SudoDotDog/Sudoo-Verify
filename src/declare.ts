edd8081d9e7067a4e67135166792dff713c75af3
WMXPY <wm@mengw.io>
2020-04-25 16:49:46 -0500

    use type specify type

--- src/declare.ts
+++ src/declare.ts
@@ -6,19 +6,24 @@
 
 export type VerifyStructure = {
 
-    readonly type: VerifyObjectStructure
-    | VerifyArrayStructure
-    | VerifyEndStructure;
+    readonly type: 'string';
+} | {
+
+    readonly type: 'number';
+} | {
+
+    readonly type: 'boolean';
+} | {
+
+    readonly type: 'map';
+    readonly map: VerifyMapStructure;
+} | {
+
+    readonly type: 'list';
+    readonly element: VerifyStructure;
 };
 
-export type VerifyArrayStructure = VerifyStructure[];
-
-export type VerifyObjectStructure = {
+export type VerifyMapStructure = {
 
     readonly [key: string]: VerifyStructure;
 };
-
-export type VerifyEndStructure =
-    'string'
-    | 'number'
-    | 'boolean';
