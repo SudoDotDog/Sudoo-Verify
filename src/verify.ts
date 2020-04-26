c24166ec468cf212c5e6625ab80242255457b766
WMXPY <wm@mengw.io>
2020-04-26 15:19:31 -0500

    partial verify

--- src/verify.ts
+++ src/verify.ts
@@ -24,17 +24,23 @@ export const verifyPattern = (pattern: Pattern, target: any): Invalid[] => {
 
 export const verifyStringPattern = (pattern: StringPattern, target: any): Invalid[] => {
 
+    const invalids: Invalid[] = [];
+
     return [];
 };
 
 export const verifyMapPattern = (pattern: MapPattern, target: any): Invalid[] => {
 
+    const invalids: Invalid[] = [];
+
     const keys: string[] = Object.keys(pattern.map);
     for (const key of keys) {
 
         const childPattern: Pattern = pattern.map[key];
-        const invalids: Invalid[] = verifyPattern(childPattern, target[key]);
+        const childInvalids: Invalid[] = verifyPattern(childPattern, target[key]);
+
+        invalids.push(...childInvalids);
     }
 
-    return [];
+    return invalids;
 };
