8267362638d6627befeac53a53d90091c3321666
WMXPY <wm@mengw.io>
2020-04-26 14:55:27 -0500

    init string verify

--- src/verifier.ts
+++ src/verifier.ts
@@ -4,14 +4,19 @@
  * @description Verifier
  */
 
+import { Pattern } from "./declare";
+
 export class Verifier {
 
-    public static create(): Verifier {
+    public static create(pattern: Pattern): Verifier {
 
-        return new Verifier();
+        return new Verifier(pattern);
     }
 
-    private constructor() {
+    private readonly _pattern: Pattern;
 
+    private constructor(pattern: Pattern) {
+
+        this._pattern = pattern;
     }
 }
---
+++ src/verify/string.ts
@@ -0,0 +1,12 @@
+/**
+ * @author WMXPY
+ * @namespace Verify_Verify
+ * @description String
+ */
+
+import { StringPattern } from "../declare";
+
+export const verifyStringPattern = (pattern: StringPattern, target: any): boolean => {
+
+    return true;
+};
