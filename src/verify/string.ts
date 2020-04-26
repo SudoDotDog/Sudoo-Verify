f6c6e0d87e6d49d8bfcb2d59d00800c262f15a07
WMXPY <wm@mengw.io>
2020-04-26 14:58:56 -0500

    use minor verify result

--- src/verify/string.ts
+++ src/verify/string.ts
@@ -4,9 +4,12 @@
  * @description String
  */
 
+import { MinorVerifyResult } from "../declare";
 import { StringPattern } from "../pattern";
 
-export const verifyStringPattern = (pattern: StringPattern, target: any): boolean => {
+export const verifyStringPattern = (pattern: StringPattern, target: any): MinorVerifyResult => {
 
-    return true;
+    return {
+        succeed: true,
+    };
 };
