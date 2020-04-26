260b1f24069f79cf0205cd6f2951e8e5b2b14e14
WMXPY <wm@mengw.io>
2020-04-26 14:53:56 -0500

    use seprated pattern info

--- src/declare.ts
+++ src/declare.ts
@@ -4,25 +4,43 @@
  * @description Declare
  */
 
-export type Pattern = {
+export type StringPattern = {
 
     readonly type: 'string';
-} | {
+};
+
+export type NumberPattern = {
 
     readonly type: 'number';
-} | {
+};
+
+export type BooleanPattern = {
 
     readonly type: 'boolean';
-} | {
+};
+
+export type MapPattern = {
 
     readonly type: 'map';
     readonly map: Record<string, Pattern>;
-} | {
+};
+
+export type ListPattern = {
 
     readonly type: 'list';
     readonly element: Pattern;
-} | {
+};
+
+export type ExactListPattern = {
 
     readonly type: 'exact-list';
     readonly list: Pattern[];
 };
+
+export type Pattern =
+    StringPattern
+    | NumberPattern
+    | BooleanPattern
+    | MapPattern
+    | ListPattern
+    | ExactListPattern;
