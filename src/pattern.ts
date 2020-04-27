5f3285c47782f22fe6447ea9df87e9a1151623a4
WMXPY <wm@mengw.io>
2020-04-27 14:09:33 -0500

    add optional common pattern

--- src/pattern.ts
+++ src/pattern.ts
@@ -4,38 +4,43 @@
  * @description Pattern
  */
 
+export type CommonPattern = {
+
+    readonly optional?: boolean;
+};
+
 export type StringPattern = {
 
     readonly type: 'string';
-};
+} & CommonPattern;
 
 export type NumberPattern = {
 
     readonly type: 'number';
-};
+} & CommonPattern;
 
 export type BooleanPattern = {
 
     readonly type: 'boolean';
-};
+} & CommonPattern;
 
 export type MapPattern = {
 
     readonly type: 'map';
     readonly map: Record<string, Pattern>;
-};
+} & CommonPattern;
 
 export type ListPattern = {
 
     readonly type: 'list';
     readonly element: Pattern;
-};
+} & CommonPattern;
 
 export type ExactListPattern = {
 
     readonly type: 'exact-list';
     readonly list: Pattern[];
-};
+} & CommonPattern;
 
 export type Pattern =
     StringPattern
