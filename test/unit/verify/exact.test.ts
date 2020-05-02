2b9e45edb55327c02959881fd8d10a823bf0ace4
WMXPY <wm@mengw.io>
2020-05-02 18:43:18 -0500

    add exact unit tests

---
+++ test/unit/verify/exact.test.ts
@@ -0,0 +1,43 @@
+/**
+ * @author WMXPY
+ * @namespace Verify_Verify
+ * @description Exact
+ * @override Unit
+ */
+
+import { expect } from 'chai';
+import * as Chance from 'chance';
+import { createValueInvalid, ExactPattern, Invalid, verifyExactPattern } from '../../../src';
+import { createDefaultVerifyOption } from '../../mock/verify';
+
+describe('Given a [Verify-Exact] Helper Method', (): void => {
+
+    const chance: Chance.Chance = new Chance('verify-verify-exact');
+
+    it('should be able to verify exact', (): void => {
+
+        const value: string = chance.string();
+        const pattern: ExactPattern = {
+            type: 'exact',
+            value,
+        };
+
+        const result: Invalid[] = verifyExactPattern(pattern, value, createDefaultVerifyOption(), []);
+
+        expect(result).to.be.deep.equal([]);
+    });
+
+    it('should be able to verify exact - sad path', (): void => {
+
+        const value: string = chance.string();
+        const actualValue: string = chance.string();
+        const pattern: ExactPattern = {
+            type: 'exact',
+            value,
+        };
+
+        const result: Invalid[] = verifyExactPattern(pattern, actualValue, createDefaultVerifyOption(), []);
+
+        expect(result).to.be.deep.equal([createValueInvalid(value, actualValue, [])]);
+    });
+});
