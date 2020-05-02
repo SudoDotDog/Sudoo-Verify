086c831f108421d3490eba582b78bed9a36ab2e0
WMXPY <wm@mengw.io>
2020-05-02 18:46:31 -0500

    add equals pattern

--- src/pattern.ts
+++ src/pattern.ts
@@ -86,6 +86,12 @@ export type CustomPattern = {
     readonly validate: (value: any) => boolean;
 } & CommonPattern;
 
+export type EqualsPattern<ValidateKey extends string = 'equals'> = {
+
+    readonly type: 'equals';
+    readonly equals: ValidateKey;
+} & CommonPattern;
+
 export type OrPattern = {
 
     readonly type: 'or';
@@ -120,6 +126,7 @@ export type Pattern =
     | ListPattern
     | ExactListPattern
     | CustomPattern
+    | EqualsPattern
     | OrPattern
     | AndPattern
     | ExactPattern
