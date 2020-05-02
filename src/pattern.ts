6c132df6f877eeea181e4cf08475a8915e293f40
WMXPY <wm@mengw.io>
2020-05-02 18:47:24 -0500

    remove unused

--- src/pattern.ts
+++ src/pattern.ts
@@ -86,12 +86,6 @@ export type CustomPattern = {
     readonly validate: (value: any) => boolean;
 } & CommonPattern;
 
-export type EqualsPattern<ValidateKey extends string = 'equals'> = {
-
-    readonly type: 'equals';
-    readonly equals: ValidateKey;
-} & CommonPattern;
-
 export type OrPattern = {
 
     readonly type: 'or';
@@ -126,7 +120,6 @@ export type Pattern =
     | ListPattern
     | ExactListPattern
     | CustomPattern
-    | EqualsPattern
     | OrPattern
     | AndPattern
     | ExactPattern
