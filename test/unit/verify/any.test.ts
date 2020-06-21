05b41d7192e666aa543cfb8c63d1caac621f723e
WMXPY <wm@mengw.io>
2020-06-21 17:47:39 -0500

    implement unit test for any

--- test/unit/verify/any.test.ts
+++ test/unit/verify/any.test.ts
@@ -8,7 +8,7 @@
 import { AnyPattern } from '@sudoo/pattern';
 import { expect } from 'chai';
 import * as Chance from 'chance';
-import { Invalid, verifyAnyPattern } from '../../../src';
+import { createTypeInvalid, Invalid, verifyAnyPattern } from '../../../src';
 import { createDefaultVerifyOption } from '../../mock/verify';
 
 describe('Given a [Verify-Any] Helper Method', (): void => {
@@ -24,5 +24,46 @@ describe('Given a [Verify-Any] Helper Method', (): void => {
         const result: Invalid[] = verifyAnyPattern(pattern, chance.floating(), createDefaultVerifyOption(), []);
 
         expect(result).to.be.deep.equal([]);
+    });
+
+    it('should be able to allow undefined or null', (): void => {
+
+        const pattern: AnyPattern = {
+            type: 'any',
+        };
+
+        const undefinedResult: Invalid[] = verifyAnyPattern(pattern, undefined, createDefaultVerifyOption(), []);
+        const nullResult: Invalid[] = verifyAnyPattern(pattern, null, createDefaultVerifyOption(), []);
+
+        expect(undefinedResult).to.be.deep.equal([]);
+        expect(nullResult).to.be.deep.equal([]);
+    });
+
+    it('should be able to fail undefined when banished', (): void => {
+
+        const pattern: AnyPattern = {
+            type: 'any',
+            banishUndefined: true,
+        };
+
+        const undefinedResult: Invalid[] = verifyAnyPattern(pattern, undefined, createDefaultVerifyOption(), []);
+        const nullResult: Invalid[] = verifyAnyPattern(pattern, null, createDefaultVerifyOption(), []);
+
+        expect(undefinedResult).to.be.deep.equal([createTypeInvalid('any', 'undefined', [])]);
+        expect(nullResult).to.be.deep.equal([]);
+    });
+
+    it('should be able to fail null when banished', (): void => {
+
+        const pattern: AnyPattern = {
+            type: 'any',
+            banishNull: true,
+        };
+
+        const undefinedResult: Invalid[] = verifyAnyPattern(pattern, undefined, createDefaultVerifyOption(), []);
+        const nullResult: Invalid[] = verifyAnyPattern(pattern, null, createDefaultVerifyOption(), []);
+
+        expect(undefinedResult).to.be.deep.equal([]);
+        expect(nullResult).to.be.deep.equal([createTypeInvalid('any', 'null', [])]);
     });
 });
